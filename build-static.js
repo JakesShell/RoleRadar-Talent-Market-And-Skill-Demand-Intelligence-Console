86aa7b09f1305e15953569733a5571636414fa91
Johannes <jvanro5@wgu.edu>
2026-05-23 18:21:57 +0700

    Add static build configuration for Vercel

---
+++ build-static.js
@@ -0,0 +1,15 @@
+﻿const fs = require("fs");
+const path = require("path");
+
+const distDir = path.join(__dirname, "dist");
+const filesToCopy = ["index.html", "styles.css", "app.js"];
+
+if (!fs.existsSync(distDir)) {
+  fs.mkdirSync(distDir);
+}
+
+for (const file of filesToCopy) {
+  fs.copyFileSync(path.join(__dirname, file), path.join(distDir, file));
+}
+
+console.log("RoleRadar static site built in dist/");
